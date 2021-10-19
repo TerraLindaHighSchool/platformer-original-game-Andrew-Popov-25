0fb6da07f52b7af4034f23e043195c3fab26e8ef
Andrew-Popov-25 <89489268+Andrew-Popov-25@users.noreply.github.com>
2021-10-19 15:24:23 -0700

    Level3 Completed
    3rd Level

--- Level3.java
+++ Level3.java
@@ -6,14 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Level2 extends World
+public class Level3 extends World
 {
     private final float GRAVITY = 0.0667f;
     private final int SPEED = 3;
     private final float JUMP_FORCE = 5.6f;
     private final int MAX_HEALTH = 3;
     private final int MAX_POWERUP = 3;
-    private final Class NEXT_LEVEL = Level3.class;
+    private final Class NEXT_LEVEL = WinSplash.class;
     private final GreenfootSound MUSIC = new GreenfootSound("incompetech_tribal.mp3");
 
     
@@ -22,7 +22,7 @@ public class Level2 extends World
      * Constructor for objects of class BrickWorld.
      * 
      */
-    public Level2()
+    public Level3()
     {    
         // Create a new world with 1200x800 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1, false); 
@@ -47,36 +47,40 @@ public class Level2 extends World
         addObject(door,1178,40);
         Floor floor = new Floor();
         addObject(floor, 600, 800);
-        BrickWall brickWall = new BrickWall();
-        addObject(brickWall,240,680);
-        BrickWall brickWall2 = new BrickWall();
-        addObject(brickWall2,960,680);
+        TrapDoor trapDoor = new TrapDoor(GRAVITY);
+        addObject(trapDoor,60,688);
         SmBrickWall smBrickWall = new SmBrickWall();
-        addObject(smBrickWall,540,560);
+        addObject(smBrickWall,180,688);
+        BrickWall brickWall = new BrickWall();
+        addObject(brickWall,480,688);
         SmBrickWall smBrickWall2 = new SmBrickWall();
-        addObject(smBrickWall2,660,560);
-        Bomb bomb = new Bomb(GRAVITY);
-        addObject(bomb,1069,763);
-        Powerup powerup = new Powerup();
-        addObject(powerup,1156,774);
-        BrickWall brickWall3 = new BrickWall();
-        addObject(brickWall3,960,430);
-        BrickWall brickWall4 = new BrickWall();
-        addObject(brickWall4,240,430);
-        Bomb bomb2 = new Bomb(GRAVITY);
-        addObject(bomb2,79,633);
-        Powerup powerup2 = new Powerup();
-        addObject(powerup2,24,644);
-        Bomb bomb3 = new Bomb(GRAVITY);
-        addObject(bomb3,1117,380);
-        Powerup powerup3 = new Powerup();
-        addObject(powerup3,1180,392);
+        addObject(smBrickWall2,779,688);
+        SmBrickWall smBrickWall3 = new SmBrickWall();
+        addObject(smBrickWall3,899,688);
         SmBrickWall smBrickWall4 = new SmBrickWall();
-        addObject(smBrickWall4,58,324);
+        addObject(smBrickWall4,1019,688);
         SmBrickWall smBrickWall5 = new SmBrickWall();
-        addObject(smBrickWall5,604,204);
+        addObject(smBrickWall5,588,569);
         SmBrickWall smBrickWall6 = new SmBrickWall();
-        addObject(smBrickWall6,1142,98);
+        addObject(smBrickWall6,809,370);
+        SmBrickWall smBrickWall7 = new SmBrickWall();
+        addObject(smBrickWall7,534,155);
+        SmBrickWall smBrickWall8 = new SmBrickWall();
+        addObject(smBrickWall8,118,423);
+        Powerup powerup = new Powerup();
+        addObject(powerup,114,385);
+        Powerup powerup2 = new Powerup();
+        addObject(powerup2,76,385);
+        Powerup powerup3 = new Powerup();
+        addObject(powerup3,151,385);
+        SmBrickWall smBrickWall9 = new SmBrickWall();
+        addObject(smBrickWall9,1140,98);
+        Bomb bomb = new Bomb(2);
+        addObject(bomb,853,324);
+        Bomb bomb2 = new Bomb(2);
+        addObject(bomb2,237,641);
+        Bomb bomb3 = new Bomb(2);
+        addObject(bomb3,487,108);
     }
     
     private void spawn()
